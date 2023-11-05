f2584e05e3f13740cbdde27d3db6753432a48091
git <kv@value.tt>
2023-11-04 20:02:54 -0400

    cors

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import json
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 def main():
     app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
     @app.route('/eye')
+    @cross_origin()
     def eye():
-        print(request.data)
+        print(request.get_json())
 
         return json.dumps({
             'cats': cats,
